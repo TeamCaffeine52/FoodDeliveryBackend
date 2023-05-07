d508ca6a652707e728cac7ca0c5289b4ee563335
SavaniKalekar <67328342+SavaniKalekar@users.noreply.github.com>
2023-05-07 17:02:22 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
-const Stripe = require('stripe')
+//const Stripe = require('stripe')
 
 const app = express();
 app.use(cors());
@@ -41,30 +41,18 @@ app.get("/", (req, res) => {
 
 //sign up
 app.post("/signup", async (req, res) => {
-  // res.send("working");
-  // return;
-  // console.log(req.body);
-  const { email } = req.body;
+//    res.send("working");
+//   // return;
+   console.log(req.body);
+  ///const { email } = req.body;
 
   try {
     const data = await userModel.create(req.body);
-    res.send(data);
+    res.send({ message: "Successfully sign up", alert: true ,data});
   } catch (error) {
     console.log(error);
-    res.send("already taken emailID")
-  }
-
-  // userModel.findOne({ email: email }, (err, result) => {
-  //   // console.log(result);
-  //   console.log(err);
-  //   if (result) {
-  //     res.send({ message: "Email id is already register", alert: false });
-  //   } else {
-  //     const data = userModel(req.body);
-  //     const save = data.save();
-  //     res.send({ message: "Successfully sign up", alert: true });
-  //   }
-  // });
+    res.send({ message: "Email id is already register", alert: false })
+  }  
 });
 
 //api login
@@ -78,110 +66,88 @@ app.post("/login", async (req, res) => {
   } catch(error) {
     console.log(error);
     console.log("problem to find user at login")
-    res.send("problem to find user")
+    res.send({message:"problem to find user",alert:false})
   }
-  
-  //, (err, result) => {
-  //     if (result) {
-  //       const dataSend = {
-  //         _id: result._id,
-  //         firstName: result.firstName,
-  //         lastName: result.lastName,
-  //         email: result.email,
-  //         image: result.image,
-  //       };
-  //       console.log(dataSend);
-  //       res.send({
-  //         message: "Login is successfully",
-  //         alert: true,
-  //         data: dataSend,
-  //       });
-  //     } else {
-  //       res.send({
-  //         message: "Email is not available, please sign up",
-  //         alert: false,
-  //       });
-  //     }
-  //   });
+
   });
 
   //product section
 
-  const schemaProduct = mongoose.Schema({
-    name: String,
-    category: String,
-    image: String,
-    price: String,
-    description: String,
-  });
-  const productModel = mongoose.model("product", schemaProduct)
+//   const schemaProduct = mongoose.Schema({
+//     name: String,
+//     category: String,
+//     image: String,
+//     price: String,
+//     description: String,
+//   });
+//   const productModel = mongoose.model("product", schemaProduct)
 
 
 
   //save product in data 
   //api
-  app.post("/uploadProduct", async (req, res) => {
-    // console.log(req.body)
-    const data = await productModel(req.body)
-    const datasave = await data.save()
-    res.send({ message: "Upload successfully" })
-  })
+//   app.post("/uploadProduct", async (req, res) => {
+//     // console.log(req.body)
+//     const data = await productModel(req.body)
+//     const datasave = await data.save()
+//     res.send({ message: "Upload successfully" })
+//   })
 
-  //
-  app.get("/product", async (req, res) => {
-    const data = await productModel.find({})
-    res.send(JSON.stringify(data))
-  })
+//   //
+//   app.get("/product", async (req, res) => {
+//     const data = await productModel.find({})
+//     res.send(JSON.stringify(data))
+//   })
 
   /*****payment getWay */
-  console.log(process.env.STRIPE_SECRET_KEY)
+  //console.log(process.env.STRIPE_SECRET_KEY)
 
 
-  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
+  //const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
-  app.post("/create-checkout-session", async (req, res) => {
+//   app.post("/create-checkout-session", async (req, res) => {
 
-    try {
-      const params = {
-        submit_type: 'pay',
-        mode: "payment",
-        payment_method_types: ['card'],
-        billing_address_collection: "auto",
-        shipping_options: [{ shipping_rate: "shr_1N0qDnSAq8kJSdzMvlVkJdua" }],
+//     try {
+//       const params = {
+//         submit_type: 'pay',
+//         mode: "payment",
+//         payment_method_types: ['card'],
+//         billing_address_collection: "auto",
+//         shipping_options: [{ shipping_rate: "shr_1N0qDnSAq8kJSdzMvlVkJdua" }],
 
-        line_items: req.body.map((item) => {
-          return {
-            price_data: {
-              currency: "inr",
-              product_data: {
-                name: item.name,
-                // images : [item.image]
-              },
-              unit_amount: item.price * 100,
-            },
-            adjustable_quantity: {
-              enabled: true,
-              minimum: 1,
-            },
-            quantity: item.qty
-          }
-        }),
+//         line_items: req.body.map((item) => {
+//           return {
+//             price_data: {
+//               currency: "inr",
+//               product_data: {
+//                 name: item.name,
+//                 // images : [item.image]
+//               },
+//               unit_amount: item.price * 100,
+//             },
+//             adjustable_quantity: {
+//               enabled: true,
+//               minimum: 1,
+//             },
+//             quantity: item.qty
+//           }
+//         }),
 
-        success_url: `${process.env.FRONTEND_URL}/success`,
-        cancel_url: `${process.env.FRONTEND_URL}/cancel`,
+//         success_url: `${process.env.FRONTEND_URL}/success`,
+//         cancel_url: `${process.env.FRONTEND_URL}/cancel`,
 
-      }
+//       }
 
 
-      const session = await stripe.checkout.sessions.create(params)
-      // console.log(session)
-      res.status(200).json(session.id)
-    }
-    catch (err) {
-      res.status(err.statusCode || 500).json(err.message)
-    }
+//       const session = await stripe.checkout.sessions.create(params)
+//       // console.log(session)
+//       res.status(200).json(session.id)
+//     }
+//     catch (err) {
+//       res.status(err.statusCode || 500).json(err.message)
+//     }
 
-  })
+//   })
 
 
   //server is ruuning
